a70a7207460ae39f5570b85aea8cd235b28c5d5b
Pete Bevin <pete@petebevin.com>
2014-08-12 18:47:06 -0400

    Challenge 1: base64

---
+++ 01base64.hs
@@ -0,0 +1,33 @@
+import Data.Char
+import Data.List
+import qualified Data.ByteString as B
+import Data.Word8
+
+splitBy :: Int -> [a] -> [[a]]
+splitBy _ [] = []
+splitBy n xs = take n xs : splitBy n (drop n xs)
+
+hex2ints :: String -> [Word8]
+hex2ints = map hex2int . splitBy 2
+  where hex2int [a,b] = fromIntegral $ 16 * (digitToInt a) + (digitToInt b)
+
+hex2bs :: String -> B.ByteString
+hex2bs = B.pack . hex2ints
+
+bs2base64 :: B.ByteString -> String
+bs2base64 = concat . map toBase64 . splitBy 3 . map fromIntegral . B.unpack
+
+enc64 :: Int -> Char
+enc64 = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" !!)
+
+toBase64 :: [Int] -> String
+toBase64 [a]     = (take 2 $ toBase64 [a,0,0]) ++ "=="
+toBase64 [a,b]   = (take 3 $ toBase64 [a,b,0]) ++ "="
+toBase64 [a,b,c] = map enc64 [w, x, y, z]
+  where w = a `div` 4
+        x = a `mod` 4 * 16 + b `div` 16
+        y = b `mod` 16 * 4 + c `div` 64
+        z = c `mod` 64
+
+hex2base64 :: String -> String
+hex2base64 = bs2base64 . hex2bs

80818981a05a36d499795dde67f2df33fadb4982
Pete Bevin <pete@hover.com>
2014-08-13 15:05:29 -0400

    Move solve into Xor.solveXor

--- 03solve_xor.hs
+++ 03solve_xor.hs
@@ -1,21 +1,6 @@
-import qualified Data.ByteString.Lazy as B
-import Data.Char
-import Data.List
-import Data.Function
 import Conversion
 import Xor
 
-solve :: B.ByteString -> [B.ByteString]
-solve message = reverse $ map fst $ sortBy (compare `on` snd) $ map addScore $ tries
-  where addScore try = (try, score try)
-        tries = [ decodeWithKey key message | key <- [0..255] ]
-        decodeWithKey key = fixedXor (B.pack $ repeat key)
-        score bs = sum $ map (freq . fromIntegral) $ filter printable $ B.unpack bs
-        printable ch = ch >= 32 && ch < 127
-        freq ch = case (chr ch) `elemIndex` (reverse "etaoinshrdlu") of
-                     Just f -> f
-                     Nothing -> -1
-
 main :: IO ()
-main = putStrLn $ show $ head $ solve $ hex2bs enc
+main = putStrLn $ show $ head $ solveXor $ hex2bs enc
   where enc = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
--- xor.hs
+++ xor.hs
@@ -1,9 +1,23 @@
-module Xor (fixedXor) where
+module Xor (fixedXor, solveXor) where
 
 import qualified Data.ByteString.Lazy as B
 import Data.Bits
 import Data.Char
+import Data.List
+import Data.Function
 
 fixedXor :: B.ByteString -> B.ByteString -> B.ByteString
 fixedXor as bs = B.pack $ zipWith xor (bytes as) (bytes bs)
   where bytes = map fromIntegral . B.unpack
+
+type Score = Int
+solveXor :: B.ByteString -> [(B.ByteString, Score)]
+solveXor message = reverse $ sortBy (compare `on` snd) $ map addScore $ tries
+  where addScore try = (try, score try)
+        tries = [ decodeWithKey key message | key <- [0..255] ]
+        decodeWithKey key = fixedXor (B.pack $ repeat key)
+        score bs = sum $ map (freq . fromIntegral) $ filter printable $ B.unpack bs
+        printable ch = ch >= 32 && ch < 127
+        freq ch = case (chr ch) `elemIndex` (reverse "etaoinshrdlu") of
+                     Just f -> f
+                     Nothing -> -1
